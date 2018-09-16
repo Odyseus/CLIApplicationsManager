d2b9ce9ae9d534449112a1dfa25919dba3388255
Odyseus <venusyadonis@gmail.com>
2018-09-16 04:30:45 -0300

    Updated __init__.py with application description.

--- AppData/python_modules/__init__.py
+++ AppData/python_modules/__init__.py
@@ -3,4 +3,9 @@
 
 __status__ = "(Perpetually evolving)"
 __appname__ = "CLI Applications Manager"
+__appdescription__ = "CLI utility to manage/develop other CLI utilities."
 __version__ = "Ω"
+
+
+if __name__ == "__main__":
+    pass
