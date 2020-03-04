557d4a0ae3d1cbd20255f579bfb47336b29b2bc0
Odyseus <venusyadonis@gmail.com>
2020-03-04 05:45:18 -0300

    General
    - Updated sub-modules.
    - Updated manual page.
    - Update base application.

--- AppData/data/templates/BaseApp/AppData/{{APP-SLUG-CAMEL}}App/__init__.py
+++ AppData/data/templates/BaseApp/AppData/{{APP-SLUG-CAMEL}}App/__init__.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 __status__ = "Stable"
 __appname__ = "{{APP-NAME}}"
 __appdescription__ = ""
 __version__ = "0.0.0"
+
+
+if __name__ == "__main__":
+    pass
