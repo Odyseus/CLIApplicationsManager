a4a47abed37526ce13938f2e3395a37e54f9bd1e
Odyseus <venusyadonis@gmail.com>
2021-09-27 08:18:15 -0300

    Updated Bash completions script

--- AppData/data/templates/BaseApp/AppData/data/templates/bash_completions.bash
+++ AppData/data/templates/BaseApp/AppData/data/templates/bash_completions.bash
@@ -23,37 +23,47 @@
 # in different files that may perform slightly different tasks.
 
 type "{executable_name}" &> /dev/null &&
-_list_dirs(){
-    # <3 https://stackoverflow.com/a/31603260
-    # List all directories found inside the passed folder ($1).
-    # WARNING! Fails on empty directories!
-    # If certain amount of complexity is needed, just create a Python function on
-    # the application's side that can be accessed as exemplified in the
-    # _command_to_create_update_a_file_ function.
-    (
-        cd "${1}" && \
-        set -- */; printf "%s\n" "${@%/}";
-    )
+# Function that "returns" the value of an argument that already exists in the CLI while one is
+# writing command arguments/options. Explaining why this is needed is even more complex than
+# what I just wrote! LOL
+# TODO: Create article in my own knowledge base based on the StackOverflow answer.
+# <3 https://stackoverflow.com/a/7948533
+_get_argument_value_from_cli_{current_date}(){
+    local tmp
+    tmp=$(getopt --quiet --options p: --longoptions profile: -- "${COMP_WORDS[@]}")
+
+    eval set -- "$tmp"
+
+    while true; do
+        case "$1" in
+        -p|--profile)
+            echo "$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            break
+            ;;
+        esac
+    done
 } &&
-_command_to_create_update_a_file_{current_date}(){
+# Function to call app.py (the app for which this auto-completion script is written for)
+# with a single argument that will print data (a "list" of strings separated by new lines)
+# to STDOUT.
+# I use this function basically for two tasks:
+# 1. To get a list of files from a folder that exists inside the Python application structure
+#    (AppData or UserData).
+# 2. To get data from a file which is easily parseable by Python (basically anything). In Bash
+#    I can barely parse data that's arranged with spaces/new lines.
+_function_to_easyly_get_data_{current_date}(){
     echo $(cd {full_path_to_app_folder}; ./app.py argument)
 } &&
-_get_array_of_file_content_{current_date}(){
-    local file_that_should_exist oldIFS
-    file_that_should_exist="{full_path_to_app_folder}/UserData/app_ids"
-
-    if [[ ! -f $file_that_should_exist ]]; then
-        _command_to_create_update_a_file_{current_date}
-    fi
-
-    oldIFS="$IFS"
-    IFS=$'\n'
-    echo $(<$file_that_should_exist)
-    IFS="$oldIFS"
-} &&
+# Function to decide if after the completion of a term should a space character be added or not.
+# It only works on Bash, not on Zsh. Not tested in any other shell.
 _decide_nospace_{current_date}(){
-    # Decide if after the completion of a term should a space character should be added or not.
-    # It only works on Bash, not on Zsh. Not tested in any other shell.
     if [[ ${1} == "--"*"=" ]] ; then
         type "compopt" &> /dev/null && compopt -o nospace
     fi
